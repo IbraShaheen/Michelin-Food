fc68a2e02e1288c906a2c2966bcd3b42aba5db88
IbraShaheen <engibraheem1995@gmail.com>
2023-11-13 02:18:41 +0300

    refactor Home component to track unsaved changes to avoid the site leaving or reload

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import InfoForm from "../components/InfoForm";
 import MenuForm from "../components/MenuForm";
 import MaintenanceForm from "../components/MaintenanceForm";
@@ -12,13 +12,51 @@ const Home = () => {
   const [done1, setDone1] = useState(false);
   const [done2, setDone2] = useState(false);
 
+  const [unsavedChanges, setUnsavedChanges] = useState(false);
+
+  // To check for unsaved changes whenever allData is updated
+  useEffect(() => {
+    const hasChanges = Object.values(allData).length > 0;
+    setUnsavedChanges(hasChanges);
+  }, [allData]);
+
+  // Event listener for beforeunload
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      if (unsavedChanges) {
+        const message =
+          "You have unsaved changes. Are you sure you want to leave?";
+        event.returnValue = message;
+        return message;
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [unsavedChanges]);
+
   return (
     <>
       <Hero />
 
-      <InfoForm allData={allData} setAllData={setAllData} done1={done1} setDone1={setDone1} />
+      <InfoForm
+        allData={allData}
+        setAllData={setAllData}
+        done1={done1}
+        setDone1={setDone1}
+      />
 
-      {done1 && <MenuForm allData={allData} setAllData={setAllData} done2={done2} setDone2={setDone2}  />}
+      {done1 && (
+        <MenuForm
+          allData={allData}
+          setAllData={setAllData}
+          done2={done2}
+          setDone2={setDone2}
+        />
+      )}
 
       {done2 && <MaintenanceForm allData={allData} setAllData={setAllData} />}
     </>
